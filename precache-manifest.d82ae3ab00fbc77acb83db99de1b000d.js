5821d9c64fa5a971e7be071494a38b686fe3b017
Robert McBryde <robert.mcbryde@ros.gov.uk>
2020-06-07 15:19:22 +0100

    Updates

---
+++ precache-manifest.d82ae3ab00fbc77acb83db99de1b000d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b43d8bfb3c700944f275ca327eec164",
+    "url": "/monster_rolodex/index.html"
+  },
+  {
+    "revision": "3f23c109fc4f4aa13847",
+    "url": "/monster_rolodex/static/css/main.bab1e3f9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster_rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster_rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f23c109fc4f4aa13847",
+    "url": "/monster_rolodex/static/js/main.770cba6b.chunk.js"
+  },
+  {
+    "revision": "beac3b7311da5459fe4d",
+    "url": "/monster_rolodex/static/js/runtime-main.17d25317.js"
+  }
+]);
